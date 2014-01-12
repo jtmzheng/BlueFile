925d79716f1333ecb0d82121352e92a07b8d0786
Max Zheng <jtzmax@hotmail.com>
2014-01-12 11:11:31 -0500

    - Added an ActionBar

--- src/com/example/bluefile/BTHostActivity.java
+++ src/com/example/bluefile/BTHostActivity.java
@@ -9,21 +9,21 @@ import java.util.Set;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
 import com.example.bluefile.fragment.AddFileDialog.NoticeDialogListener;
 import com.example.bluefile.fragment.BlueToothHostFragment;
 import com.example.bluefile.fragment.FileFragment;
 
 
-public class BTHostActivity extends FragmentActivity implements NoticeDialogListener {
+public class BTHostActivity extends ActionBarActivity implements NoticeDialogListener {
 	
 	private BTHostAdapter hostPageAdapter;
 	private ViewPager mViewPager;
@@ -37,8 +37,6 @@ public class BTHostActivity extends FragmentActivity implements NoticeDialogList
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.host_main);
 
 		fileFrag = new FileFragment();
@@ -51,16 +49,17 @@ public class BTHostActivity extends FragmentActivity implements NoticeDialogList
 		// Set up the ViewPager, attaching the adapter.
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(hostPageAdapter);
-		
+        
 		filesToSend = new HashSet<BTFile>();
 		
 	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
+		Log.v("onCreateOM", "Create options menu");
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.host_main, menu);
-		return true;
+		return super.onCreateOptionsMenu(menu);
 	}
 
 	/**
--- src/com/example/bluefile/MainActivity.java
+++ src/com/example/bluefile/MainActivity.java
@@ -3,9 +3,9 @@ package com.example.bluefile;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
@@ -19,7 +19,7 @@ import com.example.bluefile.fragment.ConfigFragment;
  * @author Max
  *
  */
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends ActionBarActivity {
 
 	private static FragmentTransaction fragmentTransaction;
 	private static FragmentManager fragmentManager;
@@ -29,9 +29,6 @@ public class MainActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		configFragment = new ConfigFragment();
 		
@@ -48,7 +45,7 @@ public class MainActivity extends FragmentActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
+		return super.onCreateOptionsMenu(menu);
 	}
 
 	/**
